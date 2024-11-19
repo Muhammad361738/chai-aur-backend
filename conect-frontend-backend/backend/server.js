9a94a6419271b75f8ec143af4842716072f04e7b
Muhammad361738 <ullahehsan016@gmail.com>
2024-11-20 02:08:53 +0500

    Add new topic in Backend journey

---
+++ conect-frontend-backend/backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+ 
+const app = express();
+
+app.get("/", (req,res) =>{
+    res.send("server is ready ");
+})
+// get the list of 5 jokes 
+app.get ("/api/jokes" , (req,res)=>{
+    const jokes = [
+        {
+            id : 1,
+            title: "Another joke" ,
+            content : "This is Joke "
+        },
+        {
+            id : 2,
+            title: "Another joke" ,
+            content : "This is another Joke "
+        },
+        {
+            id : 3,
+            title: "Third joke" ,
+            content : "This is third  Joke "
+        },
+        {
+            id : 4,
+            title: "Forth  joke" ,
+            content : "This is forth Joke "
+        },
+        {
+            id : 5,
+            title: "Fifth joke" ,
+            content : "This is fifth Joke "
+        },
+    ]
+    res.send(jokes)
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port , () =>{
+    console.log(`Server is ready at http://localhost:${port}`);
+})
